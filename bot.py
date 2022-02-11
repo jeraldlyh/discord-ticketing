5db8be904531224e6cd53f9b4ccd29c4d0b814ac
Jerald <jeraldssw@gmail.com>
2022-02-12 01:42:05 +0800

    perf: change bot to class based

--- bot.py
+++ bot.py
@@ -8,36 +8,38 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./google-credentials.json"
+class ModMail(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.members = True
+        super().__init__(command_prefix="-", intents=intents)
+        self.IGNORE_FILES = ["firebase", "modmail"]
+
+    def load_cogs(self):
+        for filename in os.listdir("./cogs"):
+            try:
+                if filename.endswith(".py") and filename[:-3] not in self.IGNORE_FILES:
+                    cog = f"cogs.{filename[:-3]}"
+                    self.load_extension(cog)
+                    print(f"Loaded {cog}")
+            except Exception as e:
+                print(e)
+
+    async def on_ready(self):
+        self.remove_command("help")
+        self.load_cogs()
+        await self.change_presence(
+            activity=discord.Activity(name="Your PMs", type=2)
+        )
+        time_now = datetime.datetime.now(tz=pytz.timezone("Asia/Singapore"))
+        login_time = time_now.strftime("%d-%m-%Y %I:%M %p")
+        print("-----------------")
+        print("Logged in as {0} at {1}".format(self.user.name, login_time))
+        print("-----------------")
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./google-credentials.json"
 load_dotenv()
 firebase_admin.initialize_app()
-
-intents = discord.Intents.default()
-intents.members = True
-
-bot = commands.Bot(command_prefix="-", intents=intents)
-bot.remove_command("help")
-
-@bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Activity(name="Your PMs", type=2))
-    time_now = datetime.datetime.now(tz=pytz.timezone("Asia/Singapore"))
-    login_time = time_now.strftime("%d-%m-%Y %I:%M %p")
-    print("-----------------")
-    print("Logged in as {0} at {1}".format(bot.user.name, login_time))
-    print("-----------------")
-
-
-if __name__ == "__main__":
-    ignore = ["firebase", "modmail"]
-    for filename in os.listdir("./cogs"):
-        try:
-            if filename.endswith(".py") and filename[:-3] not in ignore:
-                cog = f"cogs.{filename[:-3]}"
-                bot.load_extension(cog)
-                print(f"Loaded {cog}")
-        except Exception as e:
-            print(e)
-
-
+bot = ModMail()
 bot.run(os.getenv("BOT_TOKEN"))
